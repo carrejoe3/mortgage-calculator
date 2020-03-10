cf23dba8afa8b7ed64ee944046ef2d7fe1d31e14
Joe <carre.joe3@gmail.com>
2020-03-10 19:34:11 +0000

    changed fields/model values/ ids to appropriate versions

--- mortgage_calculator/src/components/Home.vue
+++ mortgage_calculator/src/components/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <v-container class="calculatorContainer">
     <div class="inputField">
-      <div class="label">Principle amount</div>
-      <v-text-field v-model="baseAmount" outlined type="number">
+      <div class="label">Mortgage Amount</div>
+      <v-text-field v-model="mortgageAmount" outlined type="number">
         <v-icon slot="append" color="$grey">mdi-cash-usd-outline</v-icon>
       </v-text-field>
     </div>
@@ -13,31 +13,17 @@
       </v-text-field>
     </div>
     <div class="inputField">
-      <div class="label">Calculation period</div>
-      <v-row>
-        <v-col>
-          <v-text-field v-model="calculationPeriodValue" type="number" outlined></v-text-field>
-        </v-col>
-        <v-col>
-          <v-btn-toggle v-model="selectedCalculationPeriodTypeValue" mandatory>
-            <v-btn value="1">Years</v-btn>
-            <v-btn value="12">Months</v-btn>
-          </v-btn-toggle>
-        </v-col>
-      </v-row>
-    </div>
-    <div class="inputField">
-      <div class="label">Compound interval</div>
-      <v-overflow-btn :items="dropdownCalculationPeriodOptions" item-text="name" item-value="value" v-model="calculationPeriodInterval"></v-overflow-btn>
+      <div class="label">Mortgage period (years)</div>
+      <v-text-field v-model="mortgagePeriod" outlined type="number"></v-text-field>
     </div>
     <div class="resultsSection">
-      <div class="totalBalanceContainer">
-        <div>Total balance:</div>
-        <div>{{ result | toCurrency }}</div>
+      <div class="totalCostContainer">
+        <div>Total cost of mortgage:</div>
+        <div>{{ totalCost | toCurrency }}</div>
       </div>
-      <div class="totalEarnedFromInterestContainer">
-        <div>Total earned from interest:</div>
-        <div>{{ totalEarnedFromInterest | toCurrency }}</div>
+      <div class="monthlyPaymentsContainer">
+        <div>Monthly payments:</div>
+        <div>{{ monthlyPayments | toCurrency }}</div>
       </div>
     </div>
   </v-container>
@@ -47,50 +33,16 @@
 export default {
   name: 'Calculator',
   data: () => ({
-    baseAmount: 0,
-    interestRate: 0,
-    calculationPeriodValue: 0,
-    calculationPeriodInterval: 1,
-    selectedCalculationPeriodTypeValue: '1',
-    dropdownCalculationPeriodOptions:
-    [
-      {
-        name: 'Daily',
-        value: 365
-      },
-      {
-        name: 'Monthly',
-        value: 12
-      },
-      {
-        name: 'Quarterly',
-        value: 4
-      },
-      {
-        name: 'Half yearly',
-        value: 2
-      },
-      {
-        name: 'Yearly',
-        value: 1
-      }
-    ]
+    mortgageAmount: 0,
+    interestRate: 3.92,
+    mortgagePeriod: 0
   }),
   computed: {
-    nullCheckedBaseAmount () {
-      return parseInt(this.baseAmount) == null ? 0 : this.baseAmount
+    totalCost () {
+      return 69
     },
-    calculationPeriod () {
-      return this.calculationPeriodValue / this.selectedCalculationPeriodTypeValue
-    },
-    depositTotal () {
-      return this.calculationPeriodInterval * this.calculationPeriod
-    },
-    result () {
-      return this.nullCheckedBaseAmount * Math.pow((1 + ((this.interestRate / 100) / this.calculationPeriodInterval)), (this.calculationPeriodInterval * this.calculationPeriod))
-    },
-    totalEarnedFromInterest () {
-      return this.result - this.baseAmount
+    monthlyPayments () {
+      return 69
     }
   }
 }
@@ -116,7 +68,7 @@ $grey: #50514F;
       padding-bottom: 0px;
     }
   }
-  .totalBalanceContainer, .totalEarnedFromInterestContainer {
+  .totalCostContainer, .monthlyPaymentsContainer {
     display: flex;
     justify-content: space-between;
   }
